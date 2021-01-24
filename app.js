660d1a1f91da547df5f38d5e4f446a20a74274ab
Myrmoxenus <scopocira@gmail.com>
2021-01-23 22:18:45 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,81 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+//Generates inquirer input type prompts
+function inquiryInput(readMeSectionName){
+  this.type = 'input'
+  this.name = readMeSectionName
+  this.message = readMeSectionName + ":"
+}
+//Desired sections of the ReadMe
+const readMeSections = ["Title", "Description", "Installation", "Usage", "License", "Contributing", "Tests", "Questions"]
+
+inquirer
+  .prompt([
+new inquiryInput("Title"),
+new inquiryInput("Description"),
+new inquiryInput("Installation"),
+new inquiryInput("Usage"),
+new inquiryInput("Contributing"),
+new inquiryInput("Tests"),
+new inquiryInput("Questions"),
+{
+  type: 'list',
+  name: 'License',
+  message: 'Desired license for your project?',
+  choices: ["Apache 2.0", "MIT", "GPL 3.0",]
+},
+  ])
+  .then((answers) => {
+    var badge = "test"
+  if (answers.License == "Apache 2.0"){
+    badge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  }
+  else if (answers.License == "MIT"){
+    badge = "[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  }
+  else if (answers.License == "GPL 3.0"){
+    badge = "[![License](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+  }
+    readMe = (`
+${badge}
+# ${answers.Title}
+
+## Table of Contents
+
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [License](#license)
+5. [Tests](#tests)
+6. [Questions](#questions)
+
+## Description:
+${answers.Description}
+
+## Installation: 
+${answers.Installation}
+
+## Usage:
+${answers.Usage}
+
+## License: 
+${answers.License}
+
+## Contributing: 
+[${answers.Contributing}](https://github.com/${answers.Contributing})
+
+## Tests: 
+${answers.Tests}
+
+## Questions: 
+ Further questions can be directed to my e-mail at  ${answers.Questions}
+`)
+
+
+    fs.writeFile('README.md', readMe, function(err){
+        if (err) throw err;
+        console.log('Saved!');
+    })
+
+  });
